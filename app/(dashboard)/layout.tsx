56fa53543ca62106f26023a3ee6a7cac219889bf
hamza bouyoussef <hamza8pro@gmail.com>
2025-03-19 17:38:53 +0000

    custumize layout of dashboard and also logic for Teahcer mode

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from "./_components/navbar";
 import SideBar from "./_components/sideBar";
 
 const DashboardLayout = ({
@@ -5,10 +6,15 @@ const DashboardLayout = ({
 }: { children: React.ReactNode; }) => {
     return (
         <div className="h-full">
+            <div className="h-[80px] md:pl-56 fixed inset-y-0 w-full z-50">
+                <Navbar/>
+            </div>
             <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
                 <SideBar/>
             </div>
-            {children}
+            <main className="md:pl-56 pt-[80px] h-full">
+              {children}
+            </main>
         </div>
 
     );
