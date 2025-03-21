27865b4e729f7c6aaeaadd9a9cf639469f870d16
hamza bouyoussef <hamza8pro@gmail.com>
2025-03-21 01:19:07 +0000

    Creation for the first api of POST course and handle it / UI

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CourseIdPage = ({ params }:
+    { params: { courseId: string } }
+) => {
+    return (
+        <div>
+            CourseIdPage {params.courseId}
+        </div>
+    )
+}
+
+export default CourseIdPage
