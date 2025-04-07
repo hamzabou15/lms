15d357ec652c7a80e0e38ec2c16c179f667211ce
hamza bouyoussef <hamza8pro@gmail.com>
2025-04-07 14:58:55 +0100

    Create a componenet Icon Banner mit cva logic

--- components/banner.tsx
+++ components/banner.tsx
@@ -1,17 +1,46 @@
-import { cva } from "class-variance-authority"
+import { cn } from "@/lib/utils"
+import { cva, VariantProps } from "class-variance-authority"
+import { AlertTriangle, CheckCircleIcon } from "lucide-react"
 
 
 
-const bannerVariants = cva({
-    "border text-center p-4 text-sm flex "
-})
+const bannerVariants = cva(
+    "border text-center p-4 text-sm flex w-full ",
+    {
+        variants: {
+            variant: {
+                warning: "bg-yellow-200/80 border-yellow-30 text-primary",
+                success: "bg-emerlad-700 border-emerald-800 text-secondary"
+            }
+        },
+        defaultVariants: {
+            variant: "warning"
+        }
+    }
+)
 
-const banner = () => {
-  return (
-    <div>
-            test
-    </div>
-  )
+interface BannerProps extends VariantProps<typeof bannerVariants> {
+    label: string
+}
+
+
+const iconMap = {
+    warning: AlertTriangle,
+    success: CheckCircleIcon
+}
+
+const banner = ({ label, variant }: BannerProps) => {
+
+    const Icon = iconMap[variant || "warning"]
+
+
+    return (
+        <div className={cn(bannerVariants({ variant }))}>
+            <Icon className="h-4 w-4 mr-2" />
+            {label}
+
+        </div>
+    )
 }
 
 export default banner
